484ba019b116d639e99205a9908a35ebaf0fcd65
vasu <vasupujy.patel@openxcelltechnolabs.com>
2020-02-17 16:55:03 +0530

    --Done POJO Setting

--- app/src/main/java/com/example/practicaldemo/MainActivity.kt
+++ app/src/main/java/com/example/practicaldemo/MainActivity.kt
@@ -22,6 +22,7 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var viewModel: MainViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
